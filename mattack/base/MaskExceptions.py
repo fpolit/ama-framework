540e84c5421d47f20ee44bfca2f089bfc2248e6b
glozanoa <glozanoa@uni.pe>
2021-01-11 19:33:19 -0500

    rename mattack project to hattack(hash attack manager)

--- mattack/base/MaskExceptions.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-
-class MaskError(Exception):
-    def __init__(self, mask):
-        self.mask = mask
-        self.warningMsg = f"Invalid mask: {self.mask}"
-        super().__init__(self.warningMsg)
