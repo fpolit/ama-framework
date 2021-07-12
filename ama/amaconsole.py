a3746a7650e2274b04844454ded3865b69a47d80
glozanoa <glozanoa@uni.pe>
2021-07-12 10:40:29 -0500

    add preloop hooks to init logger, module processor and init config

--- ama/amaconsole.py
+++ ama/amaconsole.py
@@ -11,6 +11,7 @@ import json
 import sys
 import argparse
 #import psycopg2
+from pathlib import Path
 import cmd2
 from cmd2 import Cmd
 from fineprint.status import print_failure, print_status
@@ -55,7 +56,6 @@ from ama.version import get_version
 
 # availables modules
 from ama.data.modules import amaModules
-from ama.utils.files import Path
 from ama.config import AMA_HOME
 
 # slurm
@@ -67,7 +67,7 @@ from ama.manager import ProcessManager, Process
 
 # logger
 import logging
-from ama.config.logger import Logger
+from ama.utils.logger import Logger
 
 class Ama(Cmd):
     """
@@ -90,47 +90,56 @@ class Ama(Cmd):
         self.prompt = "ama > "
         self.continuation_prompt = "> "
         self.default_category = Category.CORE
-        self.config = Ama.get_ama_configurations(ama_config)
+        self.ama_config = ama_config
+
+        # preloop hooks
+        self.register_preloop_hook(self.init_logger)
+        self.register_preloop_hook(self.init_config)
+        self.register_preloop_hook(self.init_module_processor)
+
         #self.db_conn = None #self.init_db_connection()
         #self.workspace = "default" # selected workspace
         self.modules = amaModules # format {NAME: MODULE_CLASS, ....}
         self.selectedModule = None # selected module with 'use' command (Instance of the module)
         self.filteredModules = [] # filtered modules(format: [(#, MODULE_CLASS), ...])
-        self.manager = ProcessManager()
         self.gvalues = {} # global values(format {OPTION_NAME: OPTION_VALUE, ...})
 
-        ## settable options
+
+    def init_module_processor(self) -> None:
+        self.manager = ProcessManager(logfile=self.logfile, loglevel=self.loglevel)
+        self.logger.info("Init thread to process submitted modules")
+        module_processor = threading.Thread(target=self.manager.process, daemon=True)
+        module_processor.start()
+
+    def init_logger(self) -> None:
         self.logfile = Path.joinpath(AMA_HOME, "log/ama.log")
         self.loglevel = logging.DEBUG
-
-        ## logging
-        self.logger = Logger(__name__, filelog=self.logfile, level=self.loglevel,
-                             formatlog='[%(asctime)s] %(name)s - %(levelname)s - %(message)s')
+        self.logger = Logger(__name__, logfile=self.logfile, level=self.loglevel,
+                             logformat='[%(asctime)s] %(name)s - %(levelname)s - %(message)s')
         self.logger.add_handler(handler_type=logging.FileHandler)
+        self.logger.info(f"Logger was created: level={self.loglevel}, logfile={self.logfile}")
 
-
-    @staticmethod
-    def get_ama_configurations(ama_config:Path):
+    def init_config(self) -> None:
         try:
-            configurations = None
+            config = None
 
-            if not os.path.exists(ama_config):
+            if not os.path.exists(self.ama_config):
                 raise Exception(f"Ama configuration file didn't exist: {ama_config}")
 
-            with open(ama_config) as config:
-                configurations = json.load(config)
+            with open(self.ama_config) as config_file:
+                config = json.load(config_file)
 
-            if db_credentials := configurations["db_credentials_file"]:
-                configurations["db_credentials_file"] = Path(db_credentials)
+            if db_creds := config["db_credentials_file"]:
+                config["db_credentials_file"] = Path(db_creds)
 
-            if slurm_conf_file := configurations["slurm_conf_file"]:
-                configurations["slurm_conf_file"] = Path(slurm_conf_file)
-
-            return configurations
+            self.config = config
+            self.logger.info(f"Ama configuration file was read: {self.ama_config}")
 
         except Exception as error:
-            #Cmd.pexcept(error)
-            print_status("Using default ama configuration")
+            self.logger.warninig(f"Error while reading ama config file: {self.ama_config}")
+            self.logger.pexcept(error)
+            print_status("Using default configuration")
+            self.config = {}
 
 
     # def init_db_connection(self):
@@ -152,33 +161,11 @@ class Ama(Cmd):
     #     finally:
     #         return db_conn
 
-
-    # def init_slurm_config(self):
-    #     #import pdb;pdb.set_trace()
-    #     try:
-
-    #         slurm_config_file = None
-    #         if self.config:
-    #             slurm_config_file = self.config.get("slurm_conf_file")
-
-    #         if slurm_config_file is None:
-    #             slurm_config_file = Slurm.find_slurm_config()
-
-    #         return Slurm.parser_slurm_conf(slurm_config_file)
-
-    #     except Exception as error:
-    #         print_failure(error)
-    #         #print_failure("Error while parsing slurm configuration file")
-
 def main(argv=sys.argv[1:]):
     ama = None
     try:
         #import pdb; pdb.set_trace()
         ama = Ama()
-        ama.logger.info("Init Thread to process submitted modules")
-        attack_processor = threading.Thread(target=ama.manager.process, daemon=True)
-        attack_processor.start()
-        ama.logger.info("Init ama cmdloop")
         sys.exit(ama.cmdloop())
     except Exception as error:
         if ama:
